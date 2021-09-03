f5b2a8e35180c87823ad6dadafff501abdc3658c
Prasad Boddupalli <prasad.boddupalli@hpe.com>
2021-09-03 12:28:44 +0530

    Added a variant of sequential fetching of URLs.

--- js-tutorials/async-tut-1.js
+++ js-tutorials/async-tut-1.js
@@ -23,9 +23,23 @@ function fetchUrls(urls)
   return p.then(() => bodies)
 }
 
+function fetchUrls2(urls, bodies)
+{
+  if (urls.length === 0) {
+    return bodies
+  }
+  return fetch(urls[0])
+      .then(response => response.text())
+      .then(body => bodies.push(body))
+      .then(() => fetchUrls2(urls.slice(1), bodies))
+      .catch(error => console.log("failed to fetch url...", urls[0], error))
+      .then(() => bodies)
+}
+
 // this is the main function. I will do something with the bodies that I receive
 // from fetchUrls
 
-fetchUrls(['https://amazon.in']).then(bodies => console.log(bodies))
-// let parsedURL = new URL("https://google.com");
-// console.log(parsedURL)
+// fetchUrls(['https://amazon.in']).then(bodies => console.log(bodies))
+
+// 'https://amazon.in'
+fetchUrls2(['https://amazon.in'], []).then(bodies => console.log(bodies.length))
