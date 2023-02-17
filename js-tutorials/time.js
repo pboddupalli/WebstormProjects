c9a6c0f6b34817dd975bf227b23051f87a969aac
Prasad Boddupalli <p.boddupalli@gmail.com>
2023-02-17 23:13:35 +0530

    just one more update

---
+++ js-tutorials/time.js
@@ -0,0 +1,4 @@
+// setInterval(() => console.log("hello"), 1000)
+let someObj = {}
+someObj['name'] = 'prasad'
+console.log(someObj)
---
+++ react0/src/App1.js
@@ -0,0 +1,6 @@
+function App1() { // Function Component
+    return <h1>Current time is {new Date().toTimeString()} </h1>;
+}
+
+export default App1
+
